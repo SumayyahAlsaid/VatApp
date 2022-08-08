bc76bc9d87c04cb50ca39bfa751e5d88723f3ed6
SumayyahAlsaid <sumayyahalsaid@gmail.com>
2022-08-08 08:23:29 +0300

    VAT App - WEEK 2 Homework 1 - Tuwaiq Academy

---
+++ app/src/main/java/com/example/vatapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.vatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vatapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    // binding object
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root // reference layout file
+        setContentView(view) // link layout
+
+        binding.btnCalculateVat.setOnClickListener {
+            calculateVat()
+        }// End setOnClickListener
+    }
+
+    private fun calculateVat() {
+        TODO("Not yet implemented")
+    }
+
+}
+
