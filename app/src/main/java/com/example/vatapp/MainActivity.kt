99582cdeb85640f8c3583841bad0fe8b642bcc0e
SumayyahAlsaid <sumayyahalsaid@gmail.com>
2022-08-08 13:10:47 +0300

    Calculate the vat

--- app/src/main/java/com/example/vatapp/MainActivity.kt
+++ app/src/main/java/com/example/vatapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.vatapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vatapp.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,7 +23,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateVat() {
-        TODO("Not yet implemented")
+
+        val stringInTextField = binding.etTotalCost.text.toString()
+        val cost = stringInTextField.toDouble()
+
+        val vatPercentage = when(binding.rgVatOption.checkedRadioButtonId) {
+            R.id.rb_vat_10 -> 0.10
+            R.id.rb_vat_15 -> 0.15
+            else -> 0.20
+        }
+
+        var vat = vatPercentage * cost
+        var total = vat + cost
+
+        // check switch
+        val roundVat = binding.switchRoundUp.isChecked
+        if (roundVat){
+            total = kotlin.math.ceil(total)
+        }
+
+        // Total Formatting
+        val formattedTotal = NumberFormat.getCurrencyInstance().format(total)
+        binding.txtCostTotal.text = getString(R.string.total_amount, formattedTotal)
     }
 
 }
